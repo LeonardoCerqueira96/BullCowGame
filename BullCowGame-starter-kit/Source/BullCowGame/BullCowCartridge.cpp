6c2746abd524f26f1c8a022ca68f19214dd404ba
Leonardo Cerqueira <guy.fawkes.05111605.e@gmail.com>
2019-10-17 20:47:22 -0300

    Optimized ProcessGuess function

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -17,27 +17,7 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     }
     else
     {
-        if (Input == HiddenWord)
-        {
-            PrintLine(TEXT("You have Won!"));
-            EndGame();
-        }
-        else
-        {
-            if (--Lives <= 0) 
-            {
-                PrintLine(TEXT("You have Lost!"));
-                EndGame();
-            }
-            else
-            {
-                if (Input.Len() != HiddenWord.Len())
-                {
-                    PrintLine(TEXT("You must enter a %i letter word."), HiddenWord.Len());
-                }
-                PrintLine(TEXT("You have %i lives remaining"), Lives);
-            }
-        }       
+        ProcessGuess(Input);
     }
 }
 
@@ -52,6 +32,30 @@ void UBullCowCartridge::SetupGame()
     PrintLine(TEXT("Welcome to Bull Cows!"));
     PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
     PrintLine(TEXT("You have %i lives."), Lives);
+}
+
+void UBullCowCartridge::ProcessGuess(const FString& Guess)
+{
+    if (Guess == HiddenWord)
+    {
+        PrintLine(TEXT("You have Won!"));
+        EndGame();
+        return;
+    }
+
+    if (Guess.Len() != HiddenWord.Len())
+    {
+        PrintLine(TEXT("You must enter a %i letter word."), HiddenWord.Len());
+        return;
+    }
+
+    if (--Lives <= 0) 
+    {
+        PrintLine(TEXT("You have Lost!"));
+        EndGame();
+        return;
+    }
+    PrintLine(TEXT("You have %i lives remaining"), Lives);
 }
 
 void UBullCowCartridge::EndGame()
